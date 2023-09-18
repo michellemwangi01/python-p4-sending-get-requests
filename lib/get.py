e43725820dc7ca4fd59c03180f3df0ccb037e6c7
michellemwangi01 <michellemwangim@gmail.com>
2023-09-18 23:08:47 +0300

    completed

--- lib/get.py
+++ lib/get.py
@@ -8,3 +8,18 @@ response = requests.get(url)
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
+
+# USING GET METHOD WITH PYTHON
+# 1. `import requests`
+# 2. `url = "https://learn-co-curriculum.github.io/json-site-example/"` The URL to get data from
+# 3. `response = requests.get(url)`
+# 4. `response.text`
+
+# CONVERTING TO JSON
+# 1. `import json`
+# 2. `json.loads(response.content)`
+# 3. Organize the data if need be:
+    # print(json.dumps(json.loads(response.content), indent=4, sort_keys=True))
+
+
+
